81c0dc9dc7ce745b7491062d52599783a42ecbfb
Wael Almattar <waelsy123@gmail.com>
2023-01-04 00:10:49 +0100

    auto approve install apt packages

--- terra/main.tf
+++ terra/main.tf
@@ -37,7 +37,7 @@ cd gov_housing_watcher
 npm install --unsafe-perm
 npm run tsc
 
-sudo apt-get install libpangocairo-1.0-0 libx11-xcb1 libxcomposite1 libxcursor1 libxdamage1 libxi6 libxtst6 libnss3 libcups2 libxss1 libxrandr2 libgconf2-4 libasound2 libatk1.0-0 libgtk-3-0
+sudo apt-get install -y libpangocairo-1.0-0 libx11-xcb1 libxcomposite1 libxcursor1 libxdamage1 libxi6 libxtst6 libnss3 libcups2 libxss1 libxrandr2 libgconf2-4 libasound2 libatk1.0-0 libgtk-3-0
 sudo apt-get install -y libgbm-dev
 
 chmod 777 chat_ids.txt
